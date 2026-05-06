2ba5fbadaac9a3f081e8ce0cced254f288b5c8d6
ALEXANDR Kovalev <yakovalev1@mail.ru>
2026-05-06 11:58:57 +0300

    all push

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Todolist } from "./Todolist.tsx";
+
+function App() {
+  return (
+    <div className="app">
+      <Todolist />
+      <Todolist />
+      <Todolist />
+    </div>
+  );
+}
+
+export default App;
